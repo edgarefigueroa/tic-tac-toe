29fc93c0bc1e5022b66468018dece93e994d0f6c
Edgar Figueroa <edgarefigueroa@outlook.com>
2023-01-31 22:54:22 -0500

    components & props

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Square from "./Square";
+
+const propVariable = 'This is a prop'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square propVar={propVariable}/>
+      
     </div>
   );
 }
---
+++ tic-tac-toe/src/Square.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Square = (props) => {
+
+    return (
+        <div className="square">
+            square
+            {props.propsVar}
+        </div>
+    );
+};
+
+export default Square;
